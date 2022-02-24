dcbe5af13cf4be7496fc719a24e66fb176b76f54
Mishael David <97089344+MishaelDavid@users.noreply.github.com>
2022-02-24 10:17:24 +0000

    .

--- p4final.c
+++ p4final.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int c=0;
+  int a=0;
+  int b=1;
+  for(int i=0;i<n;i++) 
+  {  c = a;
+    a = b;
+    b = c + b;
+  }
+  return c;
+}
+void output(int n, int c)
+{
+  printf("The %dth fibonacci term is %d",n,c);
+}
+int main()
+{
+  int n,c;
+  n=input();
+  c=find_fibo(n);
+  output(n,c);
+  return 0;
+}
